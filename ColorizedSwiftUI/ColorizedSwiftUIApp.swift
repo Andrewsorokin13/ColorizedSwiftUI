b5af7326cc2e07d4735b22f086d15d684805bed7
Andrewsorokin13 <Andrewsorokin13@list.ru>
2025-03-01 15:22:43 +0300

    Initial Commit

---
+++ ColorizedSwiftUI/ColorizedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIApp.swift
+//  ColorizedSwiftUI
+//
+//  Created by Андрей Сорокин on 01.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
